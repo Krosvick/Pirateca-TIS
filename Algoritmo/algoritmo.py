736d8369d77075f892a09aca0b9d40c297d8bfd4
Gianfranco Pallero <gpalleromachuca@gmail.com>
2023-09-23 00:54:40 -0300

    librerias a usar, especificación en surprise

---
+++ Algoritmo/algoritmo.py
@@ -0,0 +1,17 @@
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from surprise import Dataset 
+from surprise import Reader
+from surprise.model_selection import train_test_split
+from surprise import accuracy
+from surprise.model_selection import GridSearchCV,RandomizedSearchCV
+from sklearn.metrics import confusion_matrix, precision_score, recall_score,classification_report
+
+#el path a los datos es relativo mientras no podamos subir la bdd
+df = pd.read_csv("C:/Users/gpall/Desktop/U/8vo semestre/Movies Dataset/ratings_small.csv")
+
+df.head()
