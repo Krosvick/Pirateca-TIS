6bc484980f1a8b3aa480e4c56060e00c0efb7ede
Krosvick <josesand99v@gmail.com>
2023-10-12 12:23:50 -0300

    fix missing cols and dotenv import in csvreader

--- csvreader.php
+++ csvreader.php
@@ -3,8 +3,10 @@
     require 'functions.php';
     require 'Database.php';
     require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
     #needed rows
-    $needed_columns = ['title','adult','genres','original_language', 'original_title','overview','release_date'];
+    $needed_columns = ['title','adult','genres','original_language', 'original_title','overview','release_date','belongs_to_collection'];
 
     $csv = new \ParseCsv\Csv();
     $csv->delimiter = ",";
@@ -40,14 +42,16 @@
             'original_title' => $row['original_title'],
             'overview' => $row['overview'],
             'genres' => $row['genres'],
-            'belongs_to_collection' => $row['belongs_to_collection'],
-            'adult' => $row['adult'],
+            #belongs_to_collection is a json
+            'belongs_to_collection' => json_encode($row['belongs_to_collection']),
+            #adult is a boolean, tranform it to 1 or 0
+            'adult' => $row['adult'] == 'True' ? 1 : 0,
             'original_language' => $row['original_language'],
             'release_date' => $row['release_date'],
         ];
     }
 
-    $db = new Database();
+    $db = new Database($_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
     foreach ($movies as $movie) {
         $db->query("INSERT INTO movies (original_title, overview, genres, belongs_to_collection, adult, original_language, release_date) VALUES (:original_title, :overview, :genres, :belongs_to_collection, :adult, :original_language, :release_date)", $movie);
     }
