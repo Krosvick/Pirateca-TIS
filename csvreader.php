29122bdf7e37bba0103daf9081cee275048b056f
José Sandoval <josesand99v@gmail.com>
2023-10-12 15:13:45 +0000

    update csvreader to upload to db

--- csvreader.php
+++ csvreader.php
@@ -1,13 +1,14 @@
 <?php
     const BASE_PATH = __DIR__.'/';
     require 'functions.php';
+    require 'Database.php';
     require_once __DIR__ . '/vendor/autoload.php';
     #needed rows
     $needed_columns = ['title','adult','genres','original_language', 'original_title','overview','release_date'];
 
     $csv = new \ParseCsv\Csv();
     $csv->delimiter = ",";
-    $csv->auto(base_path("/datasets/movies_metadata.csv"));
+    $csv->auto(base_path("/datasets/movies_metadata_cleaned.csv"));
 
     $filteredData = [];
 
@@ -16,15 +17,41 @@
         $filteredData[] = $filteredRow;
     }
 
-    $jsonData = json_encode($filteredData, JSON_PRETTY_PRINT);
+    #we are going to inser into the following tables all the data
+    #table:
+    /*CREATE TABLE `movies` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+	`original_title` varchar(255),
+	`overview` text,
+	`genres` varchar(255),
+	`belongs_to_collection` json,
+	`adult` tinyint(1),
+	`original_language` varchar(3),
+	`release_date` date,
+	PRIMARY KEY (`id`)
+    )ENGINE InnoDB,
+    CHARSET utf8mb4,
+    COLLATE utf8mb4_0900_ai_ci;
+  */
+   
+    $movies = [];
+    foreach ($filteredData as $row) {
+        $movies[] = [
+            'original_title' => $row['original_title'],
+            'overview' => $row['overview'],
+            'genres' => $row['genres'],
+            'belongs_to_collection' => $row['belongs_to_collection'],
+            'adult' => $row['adult'],
+            'original_language' => $row['original_language'],
+            'release_date' => $row['release_date'],
+        ];
+    }
 
-    // Specify the file path in the current directory
-    $file_path = base_path('/datasets/json_files/filtered_data.json');
+    $db = new Database();
+    foreach ($movies as $movie) {
+        $db->query("INSERT INTO movies (original_title, overview, genres, belongs_to_collection, adult, original_language, release_date) VALUES (:original_title, :overview, :genres, :belongs_to_collection, :adult, :original_language, :release_date)", $movie);
+    }
 
-    // Save the JSON data to the file
-    file_put_contents($file_path, $jsonData);
-
-    echo "JSON data saved to: $file_path";
 
 
 
--- utils/nullremover.py
+++ utils/nullremover.py
@@ -1,13 +1,30 @@
-#this file will load ..datasets/movies_metadata.csv and find all the rows with null values and print them
 import pandas as pd
 import numpy as np
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join('..')))
 
+unnullable_cols = ['title', 'release_date', 'genres', 'overview']
+
 def load_data():
-    df = pd.read_csv('datasets/movies_metadata.csv')
-    print(df)
+    df = pd.read_csv('datasets/movies_metadata.csv', low_memory=False)
     return df
 
-loadd_data()
+def null_remover(df):
+    for col in unnullable_cols:
+        df = df[df[col].notnull()]
+    return df
+
+def check_null(df):
+    for col in df.columns:
+        print(col, df[col].isnull().sum())
+
+
+def main():
+    df = load_data()
+    df = null_remover(df)
+    check_null(df)
+    df.to_csv('datasets/movies_metadata_cleaned.csv', index=False)
+
+if __name__ == '__main__':
+    main()
