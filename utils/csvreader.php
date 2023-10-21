44e27942ee6c37b1792ba242735effa973c26277
Krosvick <josesand99v@gmail.com>
2023-10-21 15:55:19 -0300

    refactor of csvreader

--- utils/csvreader.php
+++ utils/csvreader.php
@@ -6,10 +6,10 @@
     $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
     $dotenv->load();
     #needed rows
-    $needed_columns_movies = ['id','title','adult','genres','original_language', 'original_title','overview','release_date','belongs_to_collection'];
+    $needed_columns_movies = ['id','original_title','adult','genres','original_language','overview','release_date','belongs_to_collection'];
     $needed_columns_users = [];
 
-    $movies_values = ':id, :title, :adult, :genres, :original_language, :original_title, :overview, :release_date, :belongs_to_collection';
+    $movies_values = ':id, :original_title, :adult, :genres, :original_language, :overview, :release_date, :belongs_to_collection';
 
 
     function parseCSV($file, $needed_columns) {
@@ -27,34 +27,66 @@
     function dataToDB($data, $table, $query_parameters, $values){
         set_time_limit(0);
         $db = new Database($_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+        $error_rows = [];
         foreach ($data as $row) {
-            try {
+            #every row is an array containing the values
+            #ex: ['id' => 1, 'title' => 'movie title', ...]
+            #so we need to implode the values to make it a string
+            try{
+                implode(', ', $row);
                 $db->query("INSERT INTO $table ($query_parameters) VALUES ($values)", $row);
-            }
-            catch (Exception $e) {
+            }catch(Exception $e){
                 echo $e->getMessage();
-                echo '<br>';
                 echo '<pre>';
                 print_r($row);
                 echo '</pre>';
-                continue;
+                echo '<br>';
+                //append row into the error_rows array
+                $error_rows[] = $row;
             }
+            //save the error rows into a json file
+            file_put_contents(BASE_PATH.'/datasets/json_files/error_rows.json', json_encode($error_rows));
         }
     }
     
     function dataToArray($filtered_data, $needed_columns, $keys){
         $data_array = [];
-        foreach ($filteredData as $row) {
-            $filteredRow = array_intersect_key($row, array_flip($needed_columns));
-            $data_array[] = array_combine($keys, $filteredRow);
+        foreach ($filtered_data as $row) {
+            
+            $row = array_merge(array_flip($needed_columns), $row);
+            #then assign the values to the keys
+            $row = array_combine($keys, $row);
+            $data_array[] = $row;
         }
+        $data_array = jsonEncode($data_array, 'genres', 'belongs_to_collection');
+        $data_array = numToBoolean($data_array, 'adult');
         return $data_array;
+        
+    }
+    
+    function jsonEncode($data, ...$columns){
+        $result = [];
+        foreach ($data as $row) {
+            foreach ($columns as $column) {
+                $row[$column] = json_encode($row[$column]);
+            }
+            $result[] = $row;
+        }
+        return $result;
+    }
 
+    function numToBoolean($data, $column){
+        $result = [];
+        foreach ($data as $row) {
+            $row[$column] = $row[$column] === 'True' ? 1 : 0;
+            $result[] = $row;
+        }
+        return $result;
     }
 
     $db = new Database($_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-    $db->Delete_All('movies');
-    $movies = parseCSV(BASE_PATH.'/data/movies_metadata.csv', $needed_columns_movies);
+    $db->DeleteAllHard('movies');
+    $movies = parseCSV(BASE_PATH.'/datasets/movies_metadata_cleaned.csv', $needed_columns_movies);
     $movies = dataToArray($movies, $needed_columns_movies, $needed_columns_movies);
     dataToDB($movies, 'movies', implode(', ', $needed_columns_movies), $movies_values);
 
