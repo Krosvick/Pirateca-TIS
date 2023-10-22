19e34bb9e2baa7e72e835608b8c9cbe06b1e78c4
Krosvick <josesand99v@gmail.com>
2023-10-21 21:25:09 -0300

    simplify DAO interface

--- dao/DAO.php
+++ dao/DAO.php
@@ -9,12 +9,4 @@ interface DAOInterface {
     public function create($data);
     public function update($id, $data);
     public function delete($id);
-    public function count();
-    public function findByField($field, $value);
-    public function findAllByCriteria(array $criteria);
-    public function batchInsert(array $data);
-    public function batchUpdate(array $data);
-    public function deleteAll();
-    public function truncate();
-    public function executeQuery($sql, array $params = []);
 }
