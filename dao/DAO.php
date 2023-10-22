135c95e0afa4986058e3db71d635d46d7bf9403a
SushiDevice <you@example.com>
2023-10-22 01:43:56 -0300

    more adjustments

--- dao/DAO.php
+++ dao/DAO.php
@@ -4,9 +4,9 @@ namespace DAO;
 
 interface DAOInterface {
     public function get_some($limit, $offset);
-    public function get_all();
-    public function find($id);
-    public function create($data);
-    public function update($id, $data);
-    public function delete($id);
+    //public function get_all();
+    //public function find($id);
+    //public function create($data);
+    //public function update($id, $data);
+    //public function delete($id);
 }
