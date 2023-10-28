22550749f6d6331b719123825ae5f1c5d3144357
SushiDevice <you@example.com>
2023-10-28 18:01:59 -0300

    Testing enviroment for controllers

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,19 +1,4 @@
-  <header>
-    <!-- header inner -->
-    <div class="header">
-       <div class="container-fluid">
-          <div class="row">
-             <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                <div class="full">
-                   <div class="center-desk">
-                      <div class="logo">
-                         <a href="index.html"><img src="images/scplogo.png" alt="#" /></a>
-                      </div>
-                   </div>
-                </div>
-             </div>
-             <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                <nav class="navigation navbar navbar-expand-md navbar-dark ">
+<nav class="navigation navbar navbar-expand-md navbar-dark ">
                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                    <span class="navbar-toggler-icon"></span>
                    </button>
@@ -29,18 +14,14 @@
                             <a class="nav-link" href="glasses.html">Peliculas Gustadas</a>
                          </li>
                          <li class="nav-item d_none login_btn">
-                            <a class="nav-link" href="#">Perfil</a>
+                            <a class="nav-link" href="/">Perfil</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="/testing">Testing</a>
                          </li>
                          <li class="nav-item d_none sea_icon">
-                            <a class="nav-link" href="#"><i class="fa fa-shopping-bag" aria-hidden="true"></i><i class="fa fa-search" aria-hidden="true"></i></a>
+                            <a class="nav-link" href="/"><i class="fa fa-shopping-bag" aria-hidden="true"></i><i class="fa fa-search" aria-hidden="true"></i></a>
                          </li>
                       </ul>
                    </div>
-                </nav>
-             </div>
-          </div>
-       </div>
-    </div>
- </header>
-      <!-- end header inner -->
-    
+              </nav>
