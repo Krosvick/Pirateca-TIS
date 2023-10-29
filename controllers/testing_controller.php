6172317b90ea5e3b1b33630e6c926364017d21b7
SushiDevice <you@example.com>
2023-10-28 22:12:27 -0300

    more testing

--- controllers/testing_controller.php
+++ controllers/testing_controller.php
@@ -1,8 +1,12 @@
 <?php
-const BASE_PATH = __DIR__.'/../';
-require BASE_PATH . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+//Declaramos una cons de nombre 'BASE_PATH'
+//para evitar escribir codigo largo
+//luego la reutilizan como "BASE_PATH . '/directorio'"
+const BASE_PATH = __DIR__.'/../';
+require BASE_PATH . '/vendor/autoload.php'; //composer
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../'); //credenciales?
 $dotenv->load();
 
 require BASE_PATH . 'functions.php';
@@ -14,4 +18,8 @@ use DAO\moviesDAO;
 $peliculas = new moviesDAO();
 $result = $peliculas->get_some(3, 0);
 
+$result2 = $peliculas->find(6);
+//Llamamos a la vista para que utilice la
+//variable '$result' y la muestre en un formato
+//adecuado.
 require BASE_PATH . 'views/testing_page.php';
