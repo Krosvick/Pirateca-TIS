6b2cb6ebf83dd64330bc63a9111437f2eb62efbd
Krosvick <josesand99v@gmail.com>
2023-10-29 19:53:26 -0300

    fix api fetch from user model

--- models/User.php
+++ models/User.php
@@ -50,17 +50,19 @@ class User{
     public function get_recommended_movies($quantity): array{
         $client = new GuzzleHttp\Client();
         $movies = array();
-        $response = $client->request('GET', 'localhost:8001/recommendations?user_id='.$this->user_id.'&quantity='.$quantity);
+        $response = $client->request('GET', 'localhost:8001/recommendations?userId='.$this->user_id.'&n='.$quantity);
         $response = json_decode($response->getBody(), true);
         $movieDAO = new moviesDAO();
         #the response is a json that contains movies ids and a estimated rating for each movie
         #we need to get the movies from the database
-        foreach($response as $movie){
-            $movie = $movieDAO->find($movie['movie_id']);
-            if ($movie != null){
-                array_push($movies, $movie);
+        foreach($response as $body){
+            foreach($body as $movie){
+                $movie = $movieDAO->find($movie['movieId']);
+                if ($movie != null){
+                    array_push($movies, $movie);
+                }
             }
-        }
+        }   
         return $movies;
     }
 
