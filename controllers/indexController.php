f5504163e9a8272dba8784eabf509e4a7bfbe11f
Krosvick <josesand99v@gmail.com>
2023-10-31 15:36:51 -0300

    remove api key from controller plus changes to api fallback

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -12,32 +12,36 @@ class IndexController extends BaseController
     public function index()
     {
         $dummy_user = new User();
-        $dummy_user->user_id = 1;
+        $dummy_user->user_id = 2;
         $user_movies = $dummy_user->get_recommended_movies(6);
         $client = new Client();
         foreach($user_movies as $movie){
             $poster_url = 'https://image.tmdb.org/t/p/original'.$movie['poster_path'];
             try{
                 $response = $client->request('GET', $poster_url);
-                #throw an error always
             }catch(\Exception $e){
                 if($e->getCode() == 404){
-                    $new_poster_request = $client->request('GET', 'https://api.themoviedb.org/3/movie/'.$movie['id'].'/images', [
+                    $new_poster_request = $client->request('GET', 'https://api.themoviedb.org/3/movie/'.$movie['id'].'/images?language=en', [
                         'headers' => [
-                            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDNjYzg3YmQyZjg5YmVkZDM3MGI5MTJkYzMxZDU1MCIsInN1YiI6IjY1MGRjMzk4OTNkYjkyMDBlMTc4YWVhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.czSUap8DkmDxZy1UDMMq5angEIJz4A0SLY20WL84P28',
+                            'Authorization' => 'Bearer '. $_ENV['TMDB_API_KEY'],
                             'accept' => 'application/json',
                         ]
                     ]);
                     $new_poster_response = json_decode($new_poster_request->getBody(), true);
-                    $new_poster_url = 'https://image.tmdb.org/t/p/original'.$new_poster_response['posters'][0]['file_path'];
-                    $movie['poster_path'] = $new_poster_url;
-                    #update the movie poster path in the database
-                    $movieDAO = new moviesDAO();
-                    $movieDAO->update($movie['id'], $movie, ['poster_path']);
+                    if(count($new_poster_response['posters']) > 0){
+                        $new_poster_url = $new_poster_response['posters'][0]['file_path'];
+                        $movie['poster_path'] = $new_poster_url;
+                        #update the movie poster path in the database
+                        $movieDAO = new moviesDAO();
+                        $movieDAO->update($movie['id'], $movie, ['poster_path']);
+                    }
+                    else{
+                        #if the movie doesn't have a poster, we will use a default image
+                        $movie['poster_path'] = '';
+                    }
                 }
             }
         }
-        
         $peliculas = new moviesDAO();
         $result = $peliculas->get_some(3, 0);
         $result2 = $peliculas->find(6);
