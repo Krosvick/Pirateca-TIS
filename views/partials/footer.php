aa16ed074ec02cb1dc244aa9ea54dbae2d2019d0
abordones <allmaxbordones@gmail.com>
2023-10-30 22:49:50 -0300

    footer null

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -15,7 +15,7 @@
          <div class="container">
             <div class="row">
                <div class="col-md-12">
-                  <p>© 2023 All Rights Reserved. Design by Alonso Bordones</p>
+                  <p></p>
                </div>
             </div>
          </div>
