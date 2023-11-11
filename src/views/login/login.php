45945b0ed946492efff5a8a8d4164e2e5b68b976
abordones <allmaxbordones@gmail.com>
2023-11-11 03:48:23 -0300

    min

---
+++ src/views/login/login.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenido a Pirateca</title>
+    <link rel="stylesheet" href="styles-login.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <body>
+        <div class="bg"></div>
+        <div class="form-container">
+            <div class="login-container">
+                <form>
+                    <div class="mb-6">
+                      <label for="email" class="block mb-2 text-sm font-medium text-white dark:text-white">Email</label>
+                      <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+                    </div>
+                    <div class="mb-6">
+                      <label for="password" class="block mb-2 text-sm font-medium text-white dark:text-white">Contraseña</label>
+                      <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+
+ 
+                    <button class?="boton" type="submit" class="text-white bg-stone-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</button>
+                  </form>
+            </div>
+        </div>
+    </body>
+</body>
+</html>
