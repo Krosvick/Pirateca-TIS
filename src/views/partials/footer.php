4c86da03bbb4a57e2e68a74b76e75ee1eb5ad349
abordones <allmaxbordones@gmail.com>
2023-11-11 10:55:13 -0300

    min changes

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -15,11 +15,7 @@
          <div class="container">
             <div class="row">
                <div class="col-md-12">
-<<<<<<< HEAD
                   <p></p>
-=======
-                  <p>© 2023 All Rights Reserved. Design by nobody</p>
->>>>>>> fe48cbe78e1e99ffc0944d6795ea9f78eae9c99a
                </div>
             </div>
          </div>
