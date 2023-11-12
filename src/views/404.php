239b99eda7f069ed6491d8d582bcd06c6a4f1bc7
abordones <allmaxbordones@gmail.com>
2023-11-12 19:19:24 -0300

    search page not functional

--- src/views/404.php
+++ src/views/404.php
@@ -1,5 +1,4 @@
-<?php require 'partial_testing/head.php'; ?>
-<?php require 'partial_testing/nav.php'; ?>
+<?php require 'partials/nav.php'; ?>
 
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
--- src/views/partials/nav.php
+++ src/views/partials/nav.php
@@ -6,7 +6,7 @@
 <head>
    <meta charset="UTF-8">
    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
-   <link rel="stylesheet" href="style_nav.css">
+   <link rel="stylesheet" href="styles-movie.css">
 </head>
 <body>
     <nav class="bg-white shadow">
