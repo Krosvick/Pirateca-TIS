0ccdcc773254036a80fea7b12d7109f2462383f5
abordones <allmaxbordones@gmail.com>
2023-11-17 18:07:33 -0300

    added register page

--- src/views/index.php
+++ src/views/index.php
@@ -18,7 +18,7 @@
             <div class="flex flex-row items-center h-auto snap-x snap-mandatory overflow-x-auto">
                <?php foreach ($user_movies as $result): ?>
                <div class="flex justify-center h-screen min-w-full w-full snap-center relative my-5">
-                  <img src="<?= $result["poster_path"] ? "https://image.tmdb.org/t/p/w500".$result['poster_path'] : '/views/images/scplogo.png' ?>" alt="poster" class="h-full w-4/6 rounded-2xl hover:drop-shadow-md lg:w-1/4">
+                  <img src="<?= $result["poster_path"] ? "https://image.tmdb.org/t/p/w500".$result['poster_path'] : '/views/images/PLACEHOLDER.png' ?>" alt="poster" class="h-full w-4/6 rounded-2xl hover:drop-shadow-md lg:w-1/4">
                   <a href="/another_test?id=<?= $result['id']?>" class="absolute bottom-0 h-10 w-1/2 bg-slate-900 border-t-2 border-x-2 border-zinc-300 rounded text-white text-center lg:w-1/4">
                      <?= $result['original_title'] ?>
                   </a>
--- src/views/partials/movie_page.php
+++ src/views/partials/movie_page.php
@@ -74,7 +74,7 @@
                     </div>
                 </div>
                 <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-                    <?php displayStarRating($starsGiven); ?>
+                    <?php displaystar($starsGiven); ?>
                 </div>
                 
                 <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
