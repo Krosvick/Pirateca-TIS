cc51f1f05ef8ffd0dcb93f0bbbe81aa1968f9271
SushiDevice <you@example.com>
2023-11-18 15:24:57 -0300

    fix at search controller logic

--- src/controllers/SearchController.php
+++ src/controllers/SearchController.php
@@ -15,19 +15,19 @@ class SearchController extends BaseController
     
         public function __construct() {
             //call the parent constructor to get access to the properties and methods of the BaseController class
-            parent::__construct();
+            //parent::__construct();
         
         }
 
-        public function hello(){
-        // a model
-        $movie = new MoviesDAO();
-        
-        
-        //$data = [
-         //       'Movie' => $movie
-        //];
-        require 'src\views\partials\test.php';
+        public function search(){
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['enviar'])) {
+            $busqueda = $_POST['busqueda'];
+
+            $movie = new MoviesDAO();
+            $movie = $movie->dummytest($busqueda);
+
+        require 'src\views\partials\test.php';}
         //return $this->render("partials/test", $data);
         }
 
