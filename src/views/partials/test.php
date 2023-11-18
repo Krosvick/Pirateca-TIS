ef3c76bcaa45902aab579cdf3ae68c1db4ac42e1
SushiDevice <you@example.com>
2023-11-17 23:45:23 -0300

    playground page for testing

---
+++ src/views/partials/test.php
@@ -0,0 +1,27 @@
+<?php require 'src\views\partials\nav.php'?>
+
+
+<h1>Aqui va una prueba</h1>
+
+
+<form action="test" method="post">
+    <input type="text" name="busqueda"> <br>
+    <input type="submit" name="enviar" value="Submit">
+</form>
+
+<?php
+
+if(isset($_POST['enviar'])){
+    $busqueda = $_POST['busqueda'];
+    //$busqueda = "Driv";
+    $movie = $movie->dummytest($busqueda);
+    //dd($movie);
+    foreach($movie as $row){
+        echo $row['original_title']. '<br>';
+    }
+}
+else{
+    echo "escribe algo";
+}
+
+?>
