3bc1ce224d37253c33462e860b214839375ed214
SushiDevice <you@example.com>
2023-11-17 23:44:46 -0300

    Simple search algorithm

--- routes.php
+++ routes.php
@@ -2,4 +2,5 @@
 
 $router->addRoute('/', 'indexController@index');
 $router->addRoute('/movie/{id}', 'movieController@showMovie');
+$router->addRoute('/test', 'searchController@hello');
 
--- src/controllers/SearchController.php
+++ src/controllers/SearchController.php
@@ -1,13 +1,38 @@
 <?php
 
-require_once __DIR__ .'/modelos/peliculas.php';
+namespace Controllers;
+
+use Core\BaseController;
+use DAO\moviesDAO;
+use Models\User;
+use Models\Movie;
+use GuzzleHttp\Client;
 
 
-
-        $objPelicula = new pelicula();
+class SearchController extends BaseController
+{
+       
     
-        function buscarPelicula($id) {
-        $listaPeliculas = $objPelicula->getPeliculas();
-        require 'vista.php'; // Llama a la vista para mostrar los resultados.
-        //alo commit
+        public function __construct() {
+            //call the parent constructor to get access to the properties and methods of the BaseController class
+            parent::__construct();
+        
         }
+
+        public function hello(){
+        // a model
+        $movie = new MoviesDAO();
+        
+        
+        //$data = [
+         //       'Movie' => $movie
+        //];
+        require 'src\views\partials\test.php';
+        //return $this->render("partials/test", $data);
+        }
+
+        
+}
+
+ 
+
