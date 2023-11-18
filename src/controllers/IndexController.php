64b2eda9f3548f0b17df3e5785458bc812ed35a9
elpolloconmayo <gpalleromachuca@gmail.com>
2023-11-17 22:36:32 -0300

    Deleted movie model function from index?

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -20,42 +20,9 @@ class IndexController extends BaseController
         $this->user = new User();
         $this->user->user_id = 2;
         $this->movieModel = new Movie();
-        
+
         $user_movies = $this->movieModel->find_movies($this->user->get_recommended_movies(6));
 
-        $client = new Client();
-        foreach($user_movies as $movie){
-            /*
-
-             this code should be in model or dao
-            
-             */
-            $poster_url = 'https://image.tmdb.org/t/p/original'.$movie['poster_path'];
-            try{
-                $response = $client->request('GET', $poster_url);
-            }catch(\Exception $e){
-                if($e->getCode() == 404){
-                    $new_poster_request = $client->request('GET', 'https://api.themoviedb.org/3/movie/'.$movie['id'].'/images?language=en', [
-                        'headers' => [
-                            'Authorization' => 'Bearer '. $_ENV['TMDB_API_KEY'],
-                            'accept' => 'application/json',
-                        ]
-                    ]);
-                    $new_poster_response = json_decode($new_poster_request->getBody(), true);
-                    if(count($new_poster_response['posters']) > 0){
-                        $new_poster_url = $new_poster_response['posters'][0]['file_path'];
-                        $movie['poster_path'] = $new_poster_url;
-                        #update the movie poster path in the database
-                        $movieDAO = new moviesDAO();
-                        $movieDAO->update($movie['id'], $movie, ['poster_path']);
-                    }
-                    else{
-                        #if the movie doesn't have a poster, we will use a default image
-                        $movie['poster_path'] = '';
-                    }
-                }
-            }
-        }
         $peliculas = new moviesDAO();
         $result = $peliculas->get_some(3, 0);
         $result2 = $peliculas->find(6);
@@ -66,6 +33,6 @@ class IndexController extends BaseController
             'result2' => $result2,
             'user_movies' => $user_movies
         ];
-        return $this->render("partials/movie_page", $data);
+        return $this->render("index", $data);
     }
 }
