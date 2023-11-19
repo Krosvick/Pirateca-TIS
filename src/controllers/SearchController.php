1bc8da0521fa0c833f818e8f64eab36ff47041c7
SushiDevice <you@example.com>
2023-11-18 22:49:05 -0300

    search controller testing

--- src/controllers/SearchController.php
+++ src/controllers/SearchController.php
@@ -11,28 +11,29 @@ use GuzzleHttp\Client;
 
 class SearchController extends BaseController
 {
-       
+    private $movieModel;
     
         public function __construct() {
             //call the parent constructor to get access to the properties and methods of the BaseController class
             //parent::__construct();
-        
+            
         }
 
         public function search(){
+            $busqueda = "";
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['enviar'])) {
+                $busqueda = $_POST['busqueda'];
+                $this->movieModel = new Movie();
+               // Cambiar parametro $search por $busqueda, por algun motivo guarda el ultimo submit de la vista
+                $movie = $this->movieModel->search("ariel");
+                
+            }
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['enviar'])) {
-            $busqueda = $_POST['busqueda'];
-
-            $movie = new MoviesDAO();
-            $movie = $movie->dummytest($busqueda);
-
-        require 'src\views\partials\test.php';}
-        //return $this->render("partials/test", $data);
+            require 'src\views\partials\test.php';
         }
+        //return $this->render("partials/test", $data);
+        }       
 
-        
-}
 
  
 
