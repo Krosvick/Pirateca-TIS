081c2610c40e750b0ba1e069f23a38d743f39a70
elpolloconmayo <gpalleromachuca@gmail.com>
2023-11-19 17:45:45 -0300

    Remove unnecessary code and fix image path in nav.php

--- src/views/partials/nav.php
+++ src/views/partials/nav.php
@@ -10,11 +10,7 @@
     <nav class="bg-white shadow">
         <div class="max-w-screen-xl mx-auto px-4 py-2 flex items-center justify-between">
             <div>
-<<<<<<< HEAD
-                <img src="images/logo.png" alt="Logo" class="h-8">
-=======
                 <img src="/images/logo.png" alt="Logo" class="h-8">
->>>>>>> 644fc3c5ae0f72c220b3cc67ce1c4ee1b1de02f6
             </div>
             <div>
                 <button class="text-white hover:text-purple-800 bg-purple-700 hover:bg-purple-600 px-4 py-2 rounded transition duration-300" onclick="window.location.href='login.php'">Login</button>
