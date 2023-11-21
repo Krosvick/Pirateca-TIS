c69da0191f2d50f11314d6aa75ea6aaca44b3fc0
alomso <115682968+abordones@users.noreply.github.com>
2023-11-21 12:30:55 +0000

    js button

--- src/views/partials/movie_page.php
+++ src/views/partials/movie_page.php
@@ -37,12 +37,12 @@
         <!-- prov button -->
         <!-- on click commentary.php should pop up-->
         <div>
-            <button type="button" value="toggle" class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">Rate Movie</button>
+            <button type="button" onclick="toggleDiv()" class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">Rate Movie</button>
         </div>
     </div>
     
     <!-- rate and commentary -->
-    <div>
+    <div id="toggleDiv" class="hidden">
         <?php
             require('commentary.php')
         ?>
