0163272fc55ff9e5f5ff8e08ccbb5fe0e3322f83
Krosvick <josesand99v@gmail.com>
2023-11-25 22:56:13 -0300

    Refactor MoviePageController to use RatingsDAO

--- src/controllers/MoviePageController.php
+++ src/controllers/MoviePageController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use Core\BaseController;
 use DAO\moviesDAO;
+use DAO\RatingsDAO;
 use Models\User;
 use Models\Movie;
 use Models\Rating;
@@ -14,29 +15,30 @@ class MoviePageController extends BaseController
     private $client;
     private $movieModel;
     private $ratingModel;
+    #controllers can have DAOs, models should not
+    private $movieDAO;
+    private $ratingsDAO;
 
     public function __construct($base_url, $routeParams) {
         //call the parent constructor to get access to the properties and methods of the BaseController class
         parent::__construct(...func_get_args());
         $this->movieDAO = new moviesDAO();
-        $this->ratingModel = new Rating();
+        $this->ratingsDAO = new ratingsDAO();
     }
 
     public function MoviePage($id) {
-        $movie = $this->movieDAO->find($id, 'Models\Movie');
-        $movie->MovieDirectorRetrieval();
-        $movie->moviePosterFallback();
-        $ratings = $this->ratingModel->search_by_movie_id($id);
-        if(!$movie){
+        //this is a more truthful oop approach
+        $this->movieModel = $this->movieDAO->find($id, 'Models\Movie');
+        $this->movieModel->MovieDirectorRetrieval();
+        $this->movieModel->moviePosterFallback();
+        $ratings = $this->ratingsDAO->getByMovie($this->movieModel);
+        if(!$this->movieModel){
             $this->response->abort(404);
         }
         $data = [
-            'Movie' => $movie,
+            'Movie' => $this->movieModel,
             'Ratings' => $ratings
         ];
-
-        //data should be constructed as 2 part array, movie and ratings
-        //due to the model, data and dao interaction, this page will be the most complex yet
 
         return $this->render("partials/movie_page", $data);
         
