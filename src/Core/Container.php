5be2e1b9db6e4664309e2de2bcec2eed9862ad02
Krosvick <josesand99v@gmail.com>
2023-11-27 19:00:40 -0300

    Add Container class for dependency injection

---
+++ src/Core/Container.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core;
+
+class Container
+{
+    private $instances = [];
+
+    public function get(string $class)
+    {
+        if (!isset($this->instances[$class])) {
+            $this->instances[$class] = $this->make($class);
+        }
+
+        return $this->instances[$class];
+    }
+
+    public function set(string $class, callable $instance)
+    {
+        $this->instances[$class] = $instance;
+    }
+
+    public function make(string $class)
+    {
+        if (!isset($this->instances[$class])) {
+            throw new \Exception("No instance registered for {$class}");
+        }
+
+        $callable = $this->instances[$class];
+        return $callable($this);
+    }
+}
