d501cfe56c66a263618d91e08025c6956245f5e3
Krosvick <josesand99v@gmail.com>
2023-11-27 20:53:30 -0300

    Update routes.php and login.php

--- routes.php
+++ routes.php
@@ -6,3 +6,4 @@ $router->get('/test', 'searchController@search');
 $router->get('/test/{search}', 'searchController@imsorry');
 $router->get('/login', 'LoginController@index');
 $router->post('/login', 'LoginController@index');
+$router->post('/register', 'RegisterController@index');
--- src/views/partials/login.php
+++ src/views/partials/login.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php if(isset($error)) { ?>
             <p><?php echo $error; ?></p>
         <?php } ?>
-        <form action="" method="post">
+        <form action="/register" method="post">
           <div class="mb-6">
             <label for="email" class="block mb-2 text-sm font-medium text-white dark:text-white">Email</label>
             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com" required for="usuario">
