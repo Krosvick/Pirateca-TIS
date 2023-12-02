294ca34f27cf293ffab2c6618b40b99c3ba00812
Krosvick <josesand99v@gmail.com>
2023-12-02 17:59:03 -0300

    Add get route for register page

--- routes.php
+++ routes.php
@@ -6,5 +6,6 @@ $app->router->get('/test', 'searchController@search');
 $app->router->get('/test/{search}', 'searchController@imsorry');
 $app->router->get('/login', 'LoginController@index');
 $app->router->post('/login', 'LoginController@index');
+$app->router->get('/register', 'RegisterController@index');
 $app->router->post('/register', 'RegisterController@index');
 $app->router->delete('/movie/{id}', 'MoviePageController@MoviePage');
