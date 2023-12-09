0281e31ac55a4276f62c2d97ac6cda46e50084e3
abordones <allmaxbordones@gmail.com>
2023-12-08 23:32:28 -0300

    profile and likedpost base

---
+++ src/views/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+<div class="bg"></div>
+    <div class="form-container">
+      <div class="profile-container">
+        <h1 class="text-center text-white font-bold text-2xl"><u><?php ['user']['username']?></u></h2>
+        
+        <form action="" method="get">
+        </form>
+      </div>
+    </div>
+  </body>
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
