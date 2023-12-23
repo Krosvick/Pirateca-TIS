5e6ba5e9b03b5790206f3cb5869eb72367aecaf3
abordones <allmaxbordones@gmail.com>
2023-12-23 16:11:44 -0300

    s

--- src/views/register.php
+++ src/views/register.php
@@ -10,9 +10,9 @@
   <body>
     <div class="bg"></div>
     <div class="form-container">
-      <div class="login-container">
+      <div class="register-container">
         <div class="title-container bg-transparent p-4 mb-6">
-          <h2 class="text-white text-xl font-bold">Registro de Cuenta</h2>
+          <h2 class="text-white text-3xl font-bold text-center">Registro de Cuenta</h2>
         </div>
         <form action="/register" method="POST" class="flex flex-col">
           <div class="mb-6">
@@ -31,6 +31,8 @@
             <label for="confirm_password" class="block mb-2 text-sm font-medium text-white dark:text-white">Confirmar contraseña</label>
             <input type="password" id="password" name="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           </div>
+          <hr>
+          <p>&nbsp</p>
           <button type="submit" class="text-white bg-stone-800 hover:bg-white hover:text-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrarse</button>
         </form>
       </div>
