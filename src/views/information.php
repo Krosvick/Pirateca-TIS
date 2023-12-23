867eb4776d5d0c091548efbc4a19f3e8cf8ed09a
abordones <allmaxbordones@gmail.com>
2023-12-23 06:12:06 -0300

    small changes

--- src/views/information.php
+++ src/views/information.php
@@ -14,13 +14,13 @@
     <div class="max-w-xl mx-auto bg-white shadow-md p-6 rounded-lg">
         <h2 class="text-5xl font-bold mb-4 text-center"><u>Pirateca</u></h2>
         <p class="text-gray-700 mb-4">
-            [Aquí iría la información sobre cómo utilizar la página web.]
+            [Info]
         </p>
         <hr class="my-4 border-gray-300">
-        <h3 class="text-lg font-semibold mb-2">Contacto de los creadores:</h3>
+        <h6 class="text-lg font-semibold mb-2">Contactenos</h6>
         <ul class="text-gray-700">
-            <li>Nombre: [Nombre del creador]</li>
-            <li>Email: [Correo electrónico]</li>
+            <li>telefono</li>
+            <li>email</li>
             <!-- Agrega más información de contacto si es necesario -->
         </ul>
     </div>
