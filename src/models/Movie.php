9a7bfdb5746e2e41a0eff4f7acd12579b477f010
elpolloconmayo <gpalleromachuca@gmail.com>
2023-12-24 20:08:37 -0300

    just a line that didn't work

--- src/models/Movie.php
+++ src/models/Movie.php
@@ -23,6 +23,7 @@ class Movie extends Model{
     private $director;
     //ratings array
     private array $ratings;
+    private $moviesDAO;
 
     /**
      * Class constructor for the Movie class.
@@ -436,7 +437,7 @@ class Movie extends Model{
         $movie_credits_response = json_decode($movie_credits_request->getBody(), true);
         $movie_director = $movie_credits_response['crew'][0]['name'];
         $this->director = $movie_director;
-        $this->DAOs['tableDAO']->update($this->id, $this, ['director']);
+        $this->moviesDAO->update($this->id, $this, ['director']);
         return $movie_director;
     }
 
