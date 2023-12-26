bb6bed77c159a370a2cb1afe4d068cc37abc50fc
abordones <allmaxbordones@gmail.com>
2023-12-26 20:54:43 -0300

    div correction

--- src/views/login.php
+++ src/views/login.php
@@ -19,13 +19,13 @@
                         <span class="text-base label-text text-black">Password</span>
                     </label>
                     <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:ring-gray-500" required>
-                    <p>&nbsp</p>
-                    <hr>
-                    <button class="btn btn-block border-2 bg-purple-800 text-white hover:bg-white hover:text-purple-800 hover:border-purple-700" type="submit">Login</button>
-                    <p>&nbsp</p>
-                    <hr>
-                    <button class="btn btn-block border-2 bg-gray-800 text-white hover:bg-white hover:text-gray-800 hover:border-gray-700" onclick="window.location.href='register'">Registrarse</button>
                 </div>
+                <p>&nbsp</p>
+                <hr>
+                <button class="btn btn-block border-2 bg-purple-800 text-white hover:bg-white hover:text-purple-800 hover:border-purple-700" type="submit">Login</button>
+                <p>&nbsp</p>
+                <hr>
+                <button class="btn btn-block border-2 bg-gray-800 text-white hover:bg-white hover:text-gray-800 hover:border-gray-700" onclick="window.location.href='register'">Registrarse</button>
             </form>
         </div>
     </div>
