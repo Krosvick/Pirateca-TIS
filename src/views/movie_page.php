b8416b63110c4a6cffb0840db530c813bd1a48a3
elpolloconmayo <gpalleromachuca@gmail.com>
2023-12-26 00:51:45 -0300

    base to change later, needs work on ratings

--- src/views/movie_page.php
+++ src/views/movie_page.php
@@ -45,22 +45,22 @@ require('partials/nav.php') ?>
             
             <input type="checkbox" id="my_modal_6" class="modal-toggle" />
             <div class="modal" role="dialog">
-                <form method="POST">               
-                <div class="modal-box">
-                    <h3 class="font-bold text-lg text-black center">RATE THIS MOVIE</h3>
-                    <p class="py-4 text-black"></p>
-                    <div class="rating">
-                        <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked/>
-                        <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                <form method="post" action="">               
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg text-black center">RATE THIS MOVIE</h3>
+                        <p class="py-4 text-black"></p>
+                        <div class="rating">
+                            <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked/>
+                            <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                        </div>
+                        <p class="py-4 text-black">Review this movie!</p>
+                        <input type="text" placeholder="Write your opinion..." class="w-full px-4 py-2 rounded-lg text-gray-500 bg-white border-2 border-gray-300 outline-none">
+                        <p>&nbsp</p>
+                        <button type="submit" class="bg-purple-900 text-white px-4 py-2 rounded-lg ml-2" href="/">Rate</button>
                     </div>
-                    <p class="py-4 text-black">Review this movie!</p>
-                    <input type="text" placeholder="Write your opinion..." class="w-full px-4 py-2 rounded-lg text-gray-500 bg-white border-2 border-gray-300 outline-none">
-                    <p>&nbsp</p>
-                    <button class="bg-purple-900 text-white px-4 py-2 rounded-lg ml-2" href="/">Rate</button>
-                </div>
                 </form>
             </div>
             
