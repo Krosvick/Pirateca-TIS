ea5f3e9e82ade87f1efe816cfbf589d8301b4868
abordones <allmaxbordones@gmail.com>
2023-12-26 23:35:45 -0300

    color changes

--- src/views/register.php
+++ src/views/register.php
@@ -4,16 +4,16 @@
             <h1 class="text-3xl font-semibold text-center text-gray-700">Join Us!</h1>
             <form class="space-y-4" action="/register" method="POST">
                 <div>
-                  <label for="username" class="block mb-2 text-sm font-medium dark:text-white">Ingress username</label>
-                  <input type="text" id="username" name="username" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+                  <label for="username" class="block mb-2 text-sm text-gray-600 font-medium dark:text-white">Ingress username</label>
+                  <input type="text" id="username" name="username" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
                 </div>
                 <div>
-                  <label for="password" class="block mb-2 text-sm font-medium dark:text-white">Ingress Password</label>
-                  <input type="password" id="password" name="password" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                  <label for="password" class="block mb-2 text-sm text-gray-600 font-medium dark:text-white">Ingress Password</label>
+                  <input type="password" id="password" name="password" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                 </div>
                 <div>
-                  <label for="confirm_password" class="block mb-2 text-sm font-medium dark:text-white">Confirm Passowrd</label>
-                  <input type="password" id="password" name="confirm_password" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                  <label for="confirm_password" class="block mb-2 text-sm text-gray-600 font-medium dark:text-white">Confirm Passowrd</label>
+                  <input type="password" id="password" name="confirm_password" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                 </div>
                 <div>
                     <p>&nbsp</p>
