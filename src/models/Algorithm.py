6d462245409b4dcb03d6f4c2c4a60876a0b21fc2
elpolloconmayo <gpalleromachuca@gmail.com>
2023-12-30 20:39:31 -0300

    another minor fix

--- src/models/Algorithm.py
+++ src/models/Algorithm.py
@@ -3,7 +3,7 @@ class AlgorithmSVDpp():
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Algorithm, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(AlgorithmSVDpp, cls).__new__(cls, *args, **kwargs)
         return cls._instance
 
     def __init__(self):
