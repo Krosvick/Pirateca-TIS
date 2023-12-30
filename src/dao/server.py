bf4fe9b066a199cad903217c045ad293165a38a7
elpolloconmayo <gpalleromachuca@gmail.com>
2023-12-30 19:53:26 -0300

    new design pattern added to make the sv scalable

--- src/dao/server.py
+++ src/dao/server.py
@@ -7,9 +7,6 @@ import time
 import threading
 import sys
 import pydao
-from surprise.dump import dump, load
-from surprise.model_selection import train_test_split
-from surprise import Dataset, Reader
 import os
 
 
@@ -51,10 +48,6 @@ class SimpleAPI(BaseHTTPRequestHandler):
         
         AFTER THE FIRST LOAD EVERYTHING WILL BE DONE IN THE PLANETSCALE DB
     """
-    ratings_df = pd.read_csv('datasets/reprocessed_ratings.csv', memory_map=True)
-    model = load('algoritmo2.pkl')[1]
-    #load movies metadata but only id and title
-    movies_df = pd.read_csv('datasets/movies_metadata.csv', usecols=['id', 'title'], memory_map=True)
 
     #__init__ singleton
     def __init__(self, *args, **kwargs):
@@ -85,7 +78,7 @@ class SimpleAPI(BaseHTTPRequestHandler):
             userId = int(query_params['userId'][0])
             #print every data in same "print" function to check if it's working, all df should print sample
             n = int(query_params['n'][0])
-            top_movies = Algorithm.get_user_recommendations(userId, self.ratings_df, self.movies_df, self.model, n)
+            top_movies = Algorithm.get_user_recommendations(userId, n)
             response = {'top_movies': top_movies}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -100,7 +93,7 @@ class SimpleAPI(BaseHTTPRequestHandler):
             userId = int(query_params['userId'][0])
             n = int(query_params['n'][0])
             #data processing via algorithm
-            top_movies = Algorithm.get_user_recommendations(userId, self.ratings_df, self.movies_df, self.model, n)
+            top_movies = Algorithm.get_user_recommendations(userId, n)
             print(top_movies)
             #json serialization and response
             response = {'top_movies': top_movies}
@@ -164,29 +157,6 @@ class SimpleAPI(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(response).encode('utf-8'))
 
-    def get_new_ratings():
-        """
-        Adapter function that obtains new ratings from the database and updates the ratings_df in the SimpleAPI class.
-
-        Returns:
-        - If there are new ratings, returns a DataFrame containing the new ratings.
-        - If there are no new ratings, returns None.
-        """
-        test = pydao.DAO()
-        rows = []
-        for row in test.get_all_new():
-            user_id = row[4]
-            movie_id = row[3]
-            rating = float(row[1])
-            rows.append({'userId': user_id, 'movieId': movie_id, 'rating': rating})
-        print(rows)
-        if len(rows) > 1:
-            print('New ratings found.')
-            SimpleAPI.ratings_df = pd.concat([SimpleAPI.ratings_df, pd.DataFrame(rows)])
-            return pd.DataFrame(rows)
-        else:
-            return None
-
 
 
 import numpy as np
@@ -212,57 +182,89 @@ class NpEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return super(NpEncoder, self).default(obj)
+    
 
-def generate_model_periodically():
-    """
-    Periodically generates a recommendation model based on new ratings data.
-    
-    It continuously checks for new ratings, and if there are any, it updates the existing ratings dataframe and tunes the model using the `tune_model` function from the `Algorithm` class. The function then sleeps for a specified period of time before checking for new ratings again.
-    
-    Example Usage:
-    ```python
-    # Start generating the model periodically
-    generate_model_periodically()
-    ```
-    
-    Inputs: None
-    
-    Flow:
-    1. The function enters an infinite loop.
-    2. It calls the `get_new_ratings` function from the `SimpleAPI` class to get new ratings data.
-    3. If new ratings data is available, it updates the existing ratings dataframe by concatenating the new data.
-    4. It calls the `tune_model` function from the `Algorithm` class to tune the model using the updated ratings dataframe.
-    5. The function prints the time taken to regenerate the model.
-    6. If no new ratings data is available, the function prints a message indicating that.
-    7. The function sleeps for a specified period of time.
-    8. The loop continues from step 2.
-    
-    Outputs: None
-    """
-    while True:
-        new_ratings = SimpleAPI.get_new_ratings()
+class Trainer:
 
-        if new_ratings is not None:  # Check if there are new ratings
-            # Update the ratings dataframe (for complete use in recommendation, not complete use in tuning)
-            SimpleAPI.ratings_df = pd.concat([SimpleAPI.ratings_df, pd.DataFrame(new_ratings)])
-            SimpleAPI.ratings_df.to_csv('datasets/reprocessed_ratings.csv', index=False)
-            time1 = time.time()
-            # Tune the model with the new ratings (only using the new ratings, has own offset)
-            SimpleAPI.model = Algorithm.tune_model(SimpleAPI.ratings_df, SimpleAPI.model) 
-            time2 = time.time()
-            print('Model regenerated in ' + str(time2 - time1) + ' seconds.')
-        else:
-            print('No new ratings.')
+    _algoritmo = None
+
+    def __init__(self, algoritmo = None):
+        self._algoritmo = algoritmo
+
+    def get_new_ratings(self):
+        """
+        Adapter function that obtains new ratings from the database and convert them to the algorithm format.
+
+        Returns:
+        - If there are new ratings, returns a DataFrame containing the new ratings.
+        - If there are no new ratings, returns None.
+        """
+        dao = pydao.DAO()
+        rows = []
+        for row in dao.get_all_new():
+            user_id = row[4]
+            movie_id = row[3]
+            rating = float(row[1])
+            rows.append({'userId': user_id, 'movieId': movie_id, 'rating': rating})
         
-        # wait 100 seconds before checking for new ratings again
-        del new_ratings
-        time.sleep(100) # TODO: change to 100 in presentation
+        if len(rows) > 1:
+            print('New ratings found.')
+            self._algoritmo.ratings_df = pd.concat([self._algoritmo.ratings_df, pd.DataFrame(rows)])
+            return True
+        else:
+            return False
+
+    def generate_model_periodically(self):
+        """
+        Periodically generates a recommendation model based on new ratings data.
+        
+        It continuously checks for new ratings, and if there are any, it updates the existing ratings dataframe and tunes the model using the `tune_model` function from the `Algorithm` class. The function then sleeps for a specified period of time before checking for new ratings again.
+        
+        Example Usage:
+        ```python
+        # Start generating the model periodically
+        generate_model_periodically()
+        ```
+        
+        Inputs: None
+        
+        Flow:
+        1. The function enters an infinite loop.
+        2. It calls the `get_new_ratings` function from the `SimpleAPI` class to get new ratings data.
+        3. If new ratings data is available, it updates the existing ratings dataframe by concatenating the new data.
+        4. It calls the `tune_model` function from the `Algorithm` class to tune the model using the updated ratings dataframe.
+        5. The function prints the time taken to regenerate the model.
+        6. If no new ratings data is available, the function prints a message indicating that.
+        7. The function sleeps for a specified period of time.
+        8. The loop continues from step 2.
+        
+        Outputs: None
+        """
+        while True:
+            new_ratings = self.get_new_ratings()
+
+            if new_ratings:  # Check if there are new ratings
+                # Update the ratings dataframe (for complete use in recommendation, not complete use in tuning)
+                self._algoritmo.ratings_df.to_csv('datasets/reprocessed_ratings.csv', index=False)
+                time1 = time.time()
+                # Tune the model with the new ratings (only using the new ratings, has own offset)
+                self._algoritmo.model = self._algoritmo.tune_model() 
+                time2 = time.time()
+                print('Model regenerated in ' + str(time2 - time1) + ' seconds.')
+            else:
+                print('No new ratings.')
+            
+            # wait 100 seconds before checking for new ratings again
+            del new_ratings
+            time.sleep(100) # TODO: change to 100 in presentation
 
 if __name__ == '__main__':
     """
-    Starts a new thread that periodically generates a recommendation model based on new ratings data.
+    Starts a new thread that periodically tunes a recommendation model based on new ratings data.
     Then starts an HTTP server that listens for incoming requests and serves the API.
     Both process run in parallel and only communicate through the ratings dataframe and the model.
+    Trainer uses the strategy pattern to universally train models from different algorithms, only needs the algorithm to have the same name of methods.
+    Can thread more than one model at the same time.
 
     Example Usage:
     python script.py
@@ -273,8 +275,9 @@ if __name__ == '__main__':
     Outputs:
     None
     """
+    trainer = Trainer(Algorithm())
 
-    model_thread = threading.Thread(target=generate_model_periodically)
+    model_thread = threading.Thread(target=trainer.generate_model_periodically)
     model_thread.daemon = True
     model_thread.start()
 
--- src/models/Algorithm.py
+++ src/models/Algorithm.py
@@ -1,21 +1,26 @@
 class Algorithm():
-    import sys
-    sys.path.append('src')
-    import models.Algo_config as Algo_config
+    _instance = None
 
-    def __init__(self, algorithm):
-        """
-        Initializes an instance of the Algorithm class with the specified algorithm.
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Algorithm, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
-        Args:
-            algorithm (str): The name of the algorithm.
+    def __init__(self):
+        import sys
+        import pandas as pd
+        from surprise.dump import dump, load
+        from surprise.model_selection import train_test_split
+        from surprise import Dataset, Reader
+        sys.path.append('src')
+        import models.Algo_config as Algo_config
 
-        Returns:
-            None
-        """
-        self.algorithm = algorithm
+        # hardcoded paths for better loading performance (memory_map)
+        self.ratings_df = pd.read_csv('datasets/reprocessed_ratings.csv', memory_map=True)
+        self.model = load('algoritmo2.pkl')[1]
+        self.movies_df = pd.read_csv('datasets/movies_metadata.csv', usecols=['id', 'title'], memory_map=True)
 
-    def generate_model(df, chunk_size=100000): #all file input is in csv format, remember to save the model
+    def generate_model(self, chunk_size=100000): #all file input is in csv format, remember to save the model
         """
         Input a ratings dataframe, should contain userId (int), movieId (int) and rating (float) .
         Returns and save a SVDpp model with the best hiperparameters.
@@ -36,7 +41,7 @@ class Algorithm():
         reader = Reader()
 
         # Split the dataframe into chunks
-        df = [df[i:i + chunk_size] for i in range(0, df.shape[0], chunk_size)]
+        df = [df[i:i + chunk_size] for i in range(0, self.ratings_df.shape[0], chunk_size)]
 
         # Convert each chunk to a Dataset object
         df = [Dataset.load_from_df(chunk[['userId', 'movieId', 'rating']], reader) for chunk in df]
@@ -80,10 +85,11 @@ class Algorithm():
 
         return tuned_svd_model
     
-    def tune_model(ratings_df, tuned_svd_model,  chunk_size=100000):
+    def tune_model(self,  chunk_size=100000):
         """
         Tune the SVD model incrementally by fitting it on chunks of the ratings dataframe.
         """
+        import pandas as pd
         from surprise import Dataset 
         from surprise import Reader
         from surprise.model_selection import train_test_split
@@ -93,24 +99,27 @@ class Algorithm():
         sys.path.append('src')
         import models.Algo_config as algo_config
 
+        # load again ratings_df to get the new rows
+        self.ratings_df = pd.read_csv('datasets/reprocessed_ratings.csv', memory_map=True)
+
         OFFSET = algo_config.OFFSET
         # Define the reader
         reader = Reader()
         #save a new offset var before changing the ratings_df len
-        OFFSET1 = ratings_df.shape[0]
+        OFFSET1 = self.ratings_df.shape[0]
         # Split the dataframe into chunks starting from OFFSET
-        ratings_df = ratings_df[OFFSET:]
-        print(ratings_df.head())
-        ratings_df = [ratings_df[i:i + chunk_size] for i in range(0, ratings_df.shape[0], chunk_size)]
+        self.ratings_df = self.ratings_df[OFFSET:]
+        print(self.ratings_df.head())
+        self.ratings_df = [self.ratings_df[i:i + chunk_size] for i in range(0, self.ratings_df.shape[0], chunk_size)]
         
         algo_config.OFFSET = OFFSET1
         with open('src/models/Algo_config.py', 'w') as f:
             f.write(f'OFFSET = {OFFSET1}')
 
         # Convert each chunk to a Dataset object
-        ratings_df = [Dataset.load_from_df(chunk[['userId', 'movieId', 'rating']], reader) for chunk in ratings_df]
+        self.ratings_df = [Dataset.load_from_df(chunk[['userId', 'movieId', 'rating']], reader) for chunk in self.ratings_df]
 
-        for chunk in ratings_df:
+        for chunk in self.ratings_df:
             # Split data into training and testing sets
             train_ratings = chunk.build_full_trainset()
 
@@ -128,16 +137,12 @@ class Algorithm():
 
         return tuned_svd_model
     
-    def get_user_recommendations(user_id, ratings_df, movies_df, model, predictions, top_n=10, include_rating=True):
+    def get_user_recommendations(self, user_id, top_n=10, include_rating=True):
         """
         Generate movie recommendations for a given user based on their ratings and a trained model.
 
         Args:
             user_id (int): The ID of the user for whom recommendations are generated. If set to 1 or None, random movie recommendations are returned.
-            ratings_df (DataFrame): The dataframe containing user ratings data.
-            movies_df (DataFrame): The dataframe containing movie data.
-            model (object): The trained recommendation model.
-            predictions (object): The predictions object generated by the model.
             top_n (int, optional): The number of top recommendations to return. Defaults to 30.
             include_rating (bool, optional): Whether to include the rating in the recommendations. Defaults to True.
 
@@ -155,8 +160,8 @@ class Algorithm():
             import random
             recommendations = []
             for i in range(0, top_n):
-                # get random id from movies_df['id']
-                random_id = random.choice(movies_df['id'].values)
+                # get random id from self.movies_df['id']
+                random_id = random.choice(self.movies_df['id'].values)
                 # get random rating from 0 to 5
                 random_rating = random.uniform(0, 5)
                 recommendations.append((random_id, random_rating))
@@ -171,16 +176,16 @@ class Algorithm():
             return result
         
         # Get the list of movies the user has seen
-        user_seen_movies = ratings_df[ratings_df['userId'] == user_id]['movieId'].unique()
+        user_seen_movies = self.ratings_df[self.ratings_df['userId'] == user_id]['movieId'].unique()
         # Get the list of movies the user hasn't seen
-        unseen_movies = movies_df[~movies_df['id'].isin(user_seen_movies)]
+        unseen_movies = self.movies_df[~self.movies_df['id'].isin(user_seen_movies)]
         # Placeholder for the true rating
         true_rating = 0.0
         # Prepare the testset
         unseen_movies_testset = [(user_id, movie_id, true_rating) for movie_id in unseen_movies['id']]
 
         # Make predictions only on the unseen movies
-        predictions = model.test(unseen_movies_testset)
+        predictions = self.model.test(unseen_movies_testset)
         print(accuracy.rmse(predictions))
 
         recommendations = defaultdict(list)
@@ -191,11 +196,6 @@ class Algorithm():
             user_ratings.sort(key=lambda x: x[1], reverse=True)
             recommendations[uid] = user_ratings[:top_n]
 
-        """
-        top_recommendations is a list of tuples
-            [(318, 4.327501001837994), (4973, 4.289579757337473), (4226, 4.288369094103607), (2692, 4.2516131662793315), (923, 4.2428413377628305), (898, 4.239507008315757), (3683, 4.237841176249015), (1254, 4.237667303613109), (1283, 4.224226310670601), (58559, 4.215303761456968)]
-        i need to serialize this list of tuples into a json file
-        """
         result = []
         for movie_id, rating in recommendations[user_id]:
             if include_rating:
@@ -205,44 +205,7 @@ class Algorithm():
 
         return result
     
-    def get_liked_movies(userId, df, movies):
-        """
-        Retrieve a list of movie IDs that a specific user has liked based on their ratings.
-        Also prints the titles of the liked movies.
-
-        Args:
-            userId (int): The ID of the user for whom the liked movies are retrieved.
-            df (DataFrame): The dataframe containing user ratings data.
-            movies (DataFrame): The dataframe containing movie data.
-
-        Returns:
-            list: A list of movie IDs that the user has liked.
-        """
-        user_likes = df[(df['userId'] == userId) & (df['rating'] >= 4.0)]
-    
-        if user_likes.empty:
-            print("No liked movies found for user {}.".format(userId))
-            return []
-    
-        liked_movie_ids = user_likes['movieId'].astype(int).tolist()
-    
-        print("\n", "=" * 10, "Movies that user {} likes: ".format(userId), "=" * 10)
-    
-        liked_movies_titles = []
-    
-        for movie_id in liked_movie_ids:
-            movie_id_str = str(movie_id)
-        
-            if movie_id_str in movies['id'].astype(str).values:
-                movie_title = movies[movies['id'].astype(str) == movie_id_str]['original_title'].values[0]
-                liked_movies_titles.append(movie_title)
-    
-        for title in liked_movies_titles:
-            print(title)
-    
-        return liked_movie_ids
-    
-    def get_all_predictions(ratings_df, model):
+    def get_all_predictions(self):
         """
         Get predictions for all users and all movies.
         """
@@ -251,7 +214,7 @@ class Algorithm():
         from surprise.dump import dump
 
         reader = Reader()
-        data = Dataset.load_from_df(ratings_df[['userId', 'movieId', 'rating']], reader)
+        data = Dataset.load_from_df(self.ratings_df[['userId', 'movieId', 'rating']], reader)
         trainset = data.build_full_trainset()
-        predictions = model.test(trainset.build_testset())
-        dump('test_algorithm.pkl', algo=model, predictions=predictions)
+        predictions = self.model.test(trainset.build_testset())
+        dump('test_algorithm.pkl', algo=self.model, predictions=predictions)
